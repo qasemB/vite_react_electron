62e7ed7cefd4859b2277296c6cc81eea7089c37c
qasemB <mahdicmptr@gmail.com>
2024-12-13 00:57:44 +0330

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL('http://localhost:5173'); // آدرس پیش‌فرض Vite
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
